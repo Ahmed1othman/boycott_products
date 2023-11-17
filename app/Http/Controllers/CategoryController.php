e2f56a92ca6b6756aecdf5d663267569e1ad4c3a
dina <dina.abdallah@eg.aictec.com>
2023-11-17 22:08:19 +0200

    add image category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -7,7 +7,7 @@ use App\Models\Category;
 class CategoryController extends Controller
 {
     public function index(){
-        $companies = Category::get(['id','category_name']);
+        $companies = Category::get(['id','category_name','image']);
         return response()->apiSuccess($companies);
     }
 
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -9,4 +9,14 @@ class Category extends Model
 {
     use HasFactory;
     protected $guarded = [];
+
+    protected $appends = ['image_url'];
+    public function getImageUrlAttribute()
+    {
+        if ($this->image != null) {
+            return url('/storage/'.$this->image);
+        }else{
+            return Storage::url('no-image.png');
+        }
+    }
 }
